e1792595b966f71c7f2e6bf7993da8c7d8127dda
Christoph Stock <stockulus@icloud.com>
2021-05-12 14:13:59 +0200

    first working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+type Todo = {
+  title: string
+  done: boolean
+} 
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [newItem, setNewItem] = useState('')
+
+  const handleInputChange = (event: any) => setNewItem(event.target.value)
+  const handleAddItem = () => {
+    if (!newItem) return
+    
+    setTodos(current => [...current, { title: newItem, done: false}])
+    setNewItem('')
+  }
+  
   return (
     <div className="App">
+      <div>
+        <input type="text" value={newItem} onChange={handleInputChange} />
+        <button type="button" onClick={handleAddItem}>Add</button>
+      </div>
+      <div>
+        {todos.map((item, index) => <span key={index}>{item.title}</span>)}
+      </div>
     </div>
   );
 }
