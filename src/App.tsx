8e391138e63889ce64b02b126ac030bdacf3ed0b
Christoph Stock <stockulus@icloud.com>
2021-05-12 15:19:13 +0200

    refactor newTodo

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,47 @@ type Todo = {
   done: boolean
 } 
 
-function App() {
-  const [todos, setTodos] = useSessionStorage<Todo[]>('todos', [])
-  const [newItem, setNewItem] = useState('')
 
+type NewTodoProps = {
+  onNewTodo: (value: Todo) => void
+}
+
+function NewTodo({ onNewTodo }: NewTodoProps) {
+  const [newItem, setNewItem] = useState('')
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => setNewItem(event.target.value)
   const handleAddItem = () => {
     if (!newItem) return
-    
-    setTodos(current => [...current, { title: newItem, done: false}])
+
+    onNewTodo({ title: newItem, done: false})
     setNewItem('')
   }
-  
+
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') handleAddItem()
   }
+
+  return (
+    <div>
+      <input type="text" value={newItem} onChange={handleInputChange} onKeyDown={handleKeyDown}/>
+      <button type="button" onClick={handleAddItem} disabled={!newItem}>Add</button>
+    </div>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useSessionStorage<Todo[]>('todos', [])
+  
   const handleItemDone = (index: number) => {
     setTodos([...todos.slice(0, index), {...todos[index], done: true}, ...todos.slice(index+1)])
   } 
 
+  const handleNewTodo = (todo: Todo) => {
+    setTodos(current => [...current, todo])
+  }
+
   return (
     <div className="App">
-      <div>
-        <input type="text" value={newItem} onChange={handleInputChange} onKeyDown={handleKeyDown}/>
-        <button type="button" onClick={handleAddItem} disabled={!newItem}>Add</button>
-      </div>
+      <NewTodo onNewTodo={handleNewTodo}/>
       <div>
         {todos.map((item, index) => {
            return (
